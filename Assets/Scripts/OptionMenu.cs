6d0020adc86915331c6acd2aa0c4ead1e02c2feb
kkimwooo <kkimwooo@gmail.com>
2016-10-08 09:45:23 +0900

    옵션화면

--- Assets/Scripts/OptionMenu.cs
+++ Assets/Scripts/OptionMenu.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class BGM : MonoBehaviour {
+public class OptionMenu : MonoBehaviour {
+
+    public Slider Volume = null;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        AudioListener.volume = Volume.value;
 	}
+
+
 }
---
+++ Assets/SensitiveSlider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SensitiveSlider : MonoBehaviour {
+
+    static Slider slider= null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //민감도랑 slider.value 랑 일치시킨다!
+	}
+}
